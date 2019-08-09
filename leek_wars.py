d54fbbd460a658216c7de98033bef80f9d519929
pi <pi@pi.fr>
2019-08-09 09:23:49 +0100

    it can find number of fights available

--- leek_wars.py
+++ leek_wars.py
@@ -3,9 +3,13 @@ import globalVar
 from pipenv.utils import requests
 from function_essential import readCredential,TokenLeekwars,revokeTokenLeekwars,source_api_address,writeInFile,request_api
 from garden import getGardenForLeek,getSoloChallenge,getLeekOpponents,getAI,baddestOpponents,theBaddestOpponents
-from fight import makeFight
+from fight import makeFight,nbrFightAvailable
 
 
+#print(makeFight(globalVar.leek_id,theBaddestOpponents()['id']))
+"""
+print(request_api(source_api_address+"ai/get-farmer-ais"))
+print(request_api(source_api_address+"ai/save/23456754323456786543/code"))"""
 
-
-print(makeFight(globalVar.leek_id,theBaddestOpponents()['id']))
+print(request_api(source_api_address+"garden/get")['garden']['fights'])
+print(nbrFightAvailable())
