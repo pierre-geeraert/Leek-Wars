4e1b21c54e3741fe2a73846355e3a6bdcdc54393
pi <pi@pi.fr>
2019-08-09 09:22:03 +0100

    5d45fad5210c4282ab4c70a1 find number of fights available

--- fight.py
+++ fight.py
@@ -1,9 +1,14 @@
 from pipenv.utils import requests
 from globalVar import header
+from function_essential import request_api,source_api_address
 
 def makeFight(leek_id,target_id):
     data_out_makeFight = requests.get("https://leekwars.com/api/garden/start-solo-fight/" + str(leek_id) + "/" + str(target_id),headers=header)
     return data_out_makeFight.content.decode('utf-8')
 
-def nbrFightAvailable(token):
-    return 0
+def nbrFightAvailable():
+    try:
+        data_out_nbrFightAvailable = request_api(source_api_address + "garden/get")['garden']['fights']
+    except:
+        print("request impossible for nbrFightAvailable")
+    return data_out_nbrFightAvailable
